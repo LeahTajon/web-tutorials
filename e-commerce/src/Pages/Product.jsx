9b72d49431347ba577c65864c9bc124af3bb468c
Leah <leahtajon@gmail.com>
2023-11-06 01:30:21 +1000

    create product display page

--- e-commerce/src/Pages/Product.jsx
+++ e-commerce/src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../Context/ShopContext'
 import Breadcrumb from '../Components/Breadcrumb/Breadcrumb'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const {all_product} =  useContext(ShopContext)
@@ -11,6 +12,7 @@ const Product = () => {
   return (
     <div>
       <Breadcrumb product={product}/>    
+      <ProductDisplay product={product}/>
     </div>
   )
 }
