5ff7dd15d0fb9c9d929fcceb5903d7feff284c7b
Leah <leahtajon@gmail.com>
2023-11-06 01:08:47 +1000

    add breadcrumbs

---
+++ e-commerce/src/Components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Breadcrumb.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png';
+
+const Breadcrumb = (props) => {
+    const {product} = props;
+  return (
+    <div className='breadcrumb'>
+        HOME <img src={arrow_icon} alt="arrow" /> SHOP <img src={arrow_icon} alt="arrow icon" /> {product.category} <img src={arrow_icon} alt="" />{product.name}
+    </div>
+  )
+}
+
+export default Breadcrumb
